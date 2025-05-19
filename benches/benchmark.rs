0845c21885f3a2dbb808999ca53e2f3b36882216
Alex Orlenko <zxteam@protonmail.com>
2025-05-19 14:37:29 +0100

    Update criterion to 0.6

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,8 +1,9 @@
 use std::collections::BTreeMap;
+use std::hint::black_box;
 use std::net::IpAddr;
 use std::str::FromStr;
 
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
 use rusty_rules::{Engine, IpMatcher, RegexMatcher, Value};
 use serde_json::json;
 use vrl::compiler::{state::RuntimeState, Context, Program, TargetValue, TimeZone};
